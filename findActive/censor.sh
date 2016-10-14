fef1140ad78ff0b9edc6d548f6afcbe4083430d3
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-14 12:59:28 +1030

    All mouse L1 version

--- findActive/censor.sh
+++ findActive/censor.sh
@@ -2,13 +2,13 @@
 
 # Invoked by:
 #
-# sbatch censor.sh
+# sbatch mCensor.sh
 #
 
 #SBATCH -p batch 
 #SBATCH -N 1 
 #SBATCH -n 8 
-#SBATCH --time=1-00:00 
+#SBATCH --time=0-4:00 
 #SBATCH --mem=32GB 
 
 # Notification configuration 
@@ -23,5 +23,5 @@ module load wu-blast/2.0
 
 # replace file.fasta with your mouse L1s
 # and Repbaselibrary.fasta with the Repbase library
-censor -bprm cpus=8 -lib RepBase21.03_all_seqs.ref humanUniqueActiveL1.fasta
+censor -bprm cpus=8 -lib RepBase21.03_all_seqs.ref mouseORF1ORF2min4kbmax8kb.fasta
 #mouseUniqueL1.fasta
