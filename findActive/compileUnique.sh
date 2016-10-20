9d09ae8997e8b3f7ff4f097642c9ebcc692ab30a
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-20 22:38:53 +1030

    New add Compiles splice events and reduces to unique events. Requires findUnique.sh to make inputs, all have to exist first else the script will just exit. Finds families to run censor

---
+++ findActive/compileUnique.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# compileUnique.sh takes tables from findUnique.sh and compiles three criteria into one.
+
+species="Mouse"
+
+
+wkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split/${species}/CondensedTable
+mergeDIR=${wkDIR}/Merge
+
+cd ${wkDIR} 
+
+cp bothIntron/Unique${species}_bothIntron.txt ${mergeDIR}
+cp either/Unique${species}_either.txt ${mergeDIR}
+cp Stringent/Unique${species}_stringent.txt ${mergeDIR}
+
+cd ${mergeDIR}
+
+# Uses only the absolute genomic coordinates: Use for CENSOR
+cat *.txt | awk '{print $1 "\t" $2 "\t" $3}' | sort | uniq > ${species}SpliceL1.txt
+
+# Adds relative L1 coordinates of gap, use for thesis
+cat *.txt | sort | uniq > ${species}SpliceL1-Rel.txt
+
+echo "complete"
