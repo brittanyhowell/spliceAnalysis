c0d2b1d8be0920c6924d0e3e2009706feabe0173
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-20 22:58:13 +1030

    Now executable Now requires only change of species to make Latex table files which it puts in the appendix folder. Needs findUnique.sh and compileUnique.sh to generate necessary data.

--- findActive/makeLatex.sh
+++ findActive/makeLatex.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 ## Some helpful commands to make the final L1 splice tables into LaTeX tables.
 
-cat UniqueMouse_stringent.txt | awk '{print   $1 " & " $2 " & " $3 " & " $4 " & "  $5 " \\\\"}' > LaTeXUniqueMouse_stringent.txt
 
-cat UniqueHuman.txt | awk '{print   $1 " & " $2 " & " $3 " & " $4 " & "  $5 " \\\\"}' > LaTeXUniqueHuman_stringent.txt
+species="Human"
 
-cat UniqueHuman_either.txt | awk '{print   $1 " & " $2 " & " $3 " & " $4 " & "  $5 " \\\\"}' > LaTeXUniqueHuman_either.txt
+mergeDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/bamReading/Split/${species}/CondensedTable/Merge
+appendixDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Thesis/appendices
+
+cd ${mergeDIR}
+# Make tables from Merged data
+cat ${species}SpliceL1.txt | awk '{print   $1 " & " $2 " & " $3 " \\\\"}' > ${appendixDIR}/${species}SpliceL1.tex
+
+echo "complete"
 
